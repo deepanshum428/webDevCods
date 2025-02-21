992f595fb2c2453c4d3284624c682c83cc827acc
deepanshum428 <deepanshumalviya428@gmail.com>
2025-02-21 23:12:32 +0530

    first commit

---
+++ CSS/tailwind/demo/src/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+let a = 55;
+
+// by mistake
+a = "jwdw";
+
+console.log(a + 76);
---
+++ CSS/tailwind/demo/src/test.js
@@ -0,0 +1,21 @@
+let sum = 4 + 5;
+// console.log(sum);
+
+let isloggedin = true;
+let notloggedin = false;
+
+// if (isloggedin || notloggedin)
+// console.log("ok");
+
+// if (isloggedin || notloggedin) {
+//   for (let i = 0; i < 10; i++) {
+//     console.log("not ok");
+//   }
+// }
+
+let balance = 120;
+let anotherBalance = new Number(120);
+
+console.log(balance);
+console.log(anotherBalance.valueOf());
+console.log(typeof balance);
